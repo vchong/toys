6e8f0dfde6433f36fd4eb3aa795108f33a335e63
Daniel Thompson <daniel.thompson@linaro.org>
2014-05-19 09:58:06 +0100

    buildtest: Perform build testing on more platforms.
    Change-Id: I6c731d250bec88456fa5da8f050585d6679002cd
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- tests/powerpcbuildtest.py
+++ tests/powerpcbuildtest.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# armbuildtest.py
+# powerpcbuildtest.py
 #
 # Automatically build test a wide range of ARM kernels.
 #
@@ -9,7 +9,8 @@
 #
 # Usage:
 #   <change into a mrproper'd kernel source directory>
-#   nice ionice <path_to>/armbuildtest.py
+#   ARCH=powerpc CROSS_COMPILE=powerpc64-linux- \
+#       nice ionice <path_to>/powerpcbuildtest.py
 #
 # Note:
 #   If you don't have python3 try in python2... it will probably just work
@@ -18,17 +19,21 @@
 import os
 import unittest
 
-class TestArmKernelBuilds(unittest.TestCase):
+class TestPowerpcKernelBuilds(unittest.TestCase):
     def setUp(self):
         testname = str(self).split()[0]
         self.board = testname[5:]
-        self.objdir = 'objdir-arm-' + self.board
+        self.objdir = 'objdir-powerpc-' + self.board
 
         if (not os.access(self.objdir, os.R_OK | os.W_OK | os.X_OK)):
             os.mkdir(self.objdir)
 
-        exit_code = os.system('make O=%s %s_defconfig > %s/lastbuild.log 2>&1'
-                % (self.objdir, self.board, self.objdir))
+        config_rule = self.board
+        if config_rule.startswith('defconfig_'):
+            config_rule = 'defconfig'
+
+        exit_code = os.system('make O=%s %s > %s/lastbuild.log 2>&1'
+                % (self.objdir, config_rule, self.objdir))
         self.assertEqual(0, exit_code,
             'Initial config failed: See "%s/lastbuild.log"' % (self.objdir,))
 
@@ -75,73 +80,17 @@ class TestArmKernelBuilds(unittest.TestCase):
         self.config(('MAGIC_SYSRQ', 'MAGIC_SYSRQ_BREAK_EMULATION',
                      'KGDB', 'KGDB_KDB'))
 
-    def make(self):
-        exit_code = os.system('make -C %s -j 24 >> %s/lastbuild.log 2>&1' 
-            % (self.objdir, self.objdir))
+    def make(self, cmd=''):
+        exit_code = os.system('make -C %s -j 24 %s >> %s/lastbuild.log 2>&1' 
+            % (self.objdir, cmd, self.objdir))
         self.assertEqual(0, exit_code,
             'Build failed: See "%s/lastbuild.log"' % (self.objdir))
 
-    def test_at91_dt(self):
+    def test_defconfig(self):
         self.make()
 
-    def test_clps711x(self):
-        self.make()
-
-    def test_ep93xx(self):
-        self.make()
-        self.config(disable='DEBUG_LL')
-
-    def test_footbridge(self):
-        self.make()
-
-    def test_integrator(self):
-        self.make()
-
-    def test_ks8695(self):
-        self.make()
-
-    def test_multi_v7(self):
-        self.make()
-        self.config_modernize()
+    def test_defconfig_kgdb(self):
         self.config_kgdb()
-    
-    def test_netx(self):
         self.make()
-
-    def test_omap1(self):
-        self.make()
-
-    def test_lart(self): # ARCH_SA1100
-        self.make()
-
-    def test_spear3xx(self):
-        self.make()
-
-    def test_spear6xx(self):
-        self.make()
-
-    def test_spear13xx(self):
-        self.make()
-
-    def test_s3c2410(self):
-        self.make()
-    
-    def test_s3c6400(self):
-        self.make()
-    
-    def test_s5p64x0(self):
-        self.make()
-    
-    def test_s5pc100(self):
-        self.make()
-    
-    def test_s5pv210(self):
-        self.make()
-    
-    def test_versatile(self):
-        self.make()
-        self.config(disable='DEBUG_LL')
-        self.config_modernize()
-        self.config_kgdb()
 
 if __name__ == '__main__': unittest.main()
