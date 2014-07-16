701f960a1e3e35ce31d51c80e8c61448cf5fe536
Daniel Thompson <daniel.thompson@linaro.org>
2014-07-16 14:41:27 +0100

    versatile_verdict/b2020_verdict: Add support for FIQ boot testing
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- tests/b2020_verdict.py
+++ tests/b2020_verdict.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
-import verdict
+import sys, verdict
 
-def run():
-	verdict.build('config multi_v7_defconfig --modernize')
+def run(args=[]):
+	config = ['config multi_v7_defconfig --modernize']
+	console = 'console=ttyAS0,115200'
 
+	for arg in args:
+		if arg.startswith('CONFIG_'):
+			config.append('config --enable ' + arg)
+
+	if 'thumb' in args:
+		config.append('config --thumb')
+
+	if 'fiq' in args:
+		config.append('config --kgdb ' +
+				'--enable KGDB_FIQ --enable SERIAL_KGDB_NMI')
+		console = 'console=ttyNMI0 kgdboc=ttyAS0,115200 kgdb_fiq.enable=1'
+
+	verdict.build(config)
 	uart = verdict.telnet('agnes', 5331)
 	gdb = verdict.stlinux_arm_boot('stlinux_arm_boot ' +
 		'-t agnes:b2020stxh416:a9_0,' +
@@ -15,14 +29,21 @@ def run():
 			'uart_setup=1 ' +
 		'-L -dtb arch/arm/boot/dts/stih416-b2020.dtb ' +
 		'-b vmlinux -- ' +
-		'console=ttyAS0,115200 ' +
+		console + ' ' +
 		'root=/dev/nfs rw ' +
 		'nfsroot=192.168.1.33:/opt/debian/jessie-armel-rootfs,tcp,v3 ' +
 'ip=192.168.1.39:192.168.1.33:192.168.1.1:255.255.255.0:curry:eth0:off ' +
-		'ipconfdelay=0')
+		'ipconfdelay=0 clk_ignore_unused')
 
 	verdict.expect_systemd_boot(uart)
+	if 'fiq' in args:
+		verdict.expect_nmi_debugger(uart)
+	verdict.expect_login_prompt(uart)
+	if 'fiq' in args:
+		verdict.expect_nmi_debugger(uart)
+		verdict.expect_nmi_debugger(uart)
+
 	verdict.good((uart, gdb))
 
 if __name__ == '__main__':
-	run()
+	run(sys.argv[1:])
--- tests/versatile_verdict.py
+++ tests/versatile_verdict.py
@@ -1,21 +1,36 @@
 #!/usr/bin/env python
 
-import verdict
+import sys, verdict
 
-def run():
+def run(args=[]):
 	verdict.build('config versatile_defconfig --modernize --nfs')
+	console = 'console=ttyAMA0,115200 kgdboc=ttyAMA0'
+
+	if 'fiq' in args:
+		verdict.build('config --kgdb --enable PRINTK_TIME ' +
+				'--enable KGDB_FIQ --enable SERIAL_KGDB_NMI')
+		console = 'console=ttyNMI0 kgdboc=ttyAMA0,115200 kgdb_fiq.enable=1'
+
 	qemu = verdict.qemu('qemu-system-arm ' + 
 		'-M versatilepb -m 256M -nographic ' +
 		'-serial tcp::5331,server ' +
 		'-kernel arch/arm/boot/zImage ' +
-		'-append "console=ttyAMA0,115200 kgdboc=ttyAMA0 rw ' +
-		'nfsroot=10.0.2.2:/opt/debian/jessie-armel-rootfs,v3 ' +
+		'-append "' + console + ' '+
+		'rw nfsroot=10.0.2.2:/opt/debian/jessie-armel-rootfs,v3 ' +
 		'ip=dhcp"')
+
 	uart = verdict.telnet('localhost', 5331)
+
 	verdict.expect_slow_replies(uart)
 	verdict.expect_systemd_boot(uart, 'Uncompressing Linux...')
+	if 'fiq' in args:
+		verdict.expect_nmi_debugger(uart)
+	verdict.expect_login_prompt(uart)
+	#if 'fiq' in args:
+	#	verdict.expect_nmi_debugger(uart)
+	#	verdict.expect_nmi_debugger(uart)
 
 	verdict.good((uart, qemu))
 
 if __name__ == '__main__':
-	run()
+	run(sys.argv[1:])
