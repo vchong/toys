7327d1c46a9e2911c648fa4b7640dc1994834e73
Daniel Thompson <daniel.thompson@linaro.org>
2017-02-06 16:33:58 +0000

    glance: Many new commands
    Add passwd and weekly. Significantly extend chart and convert
    --json into a conditional argument.
    
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- lib/python/toys/chart.py
+++ lib/python/toys/chart.py
@@ -7,6 +7,8 @@ Works best with data sets prepared using toys.collect
 import hashlib
 import matplotlib.pyplot as plt
 
+import toys.collect as collect
+
 def get_colour(s):
 	override = {
 		'lava' : 'orange',
@@ -22,7 +24,7 @@ def get_colour(s):
 
 	return '#{}'.format(hashlib.md5(str(s).encode('UTF-8')).hexdigest()[-6:])
 
-def stacked_bar_chart(things, filename, title=None, xlabel=None, ylabel=None):
+def stacked_barchart(things, filename, title=None, xlabel=None, ylabel=None):
 	x_labels = sorted(things.keys())
 
 	legend_labels = set()
@@ -58,4 +60,18 @@ def stacked_bar_chart(things, filename, title=None, xlabel=None, ylabel=None):
 	plt.xticks([ x+0.5 for x in left ], x_labels, rotation=90)
 	lgd = plt.legend(plots, legend_labels, bbox_to_anchor=(1.05, 1), loc=2)
 	plt.savefig(filename, bbox_extra_artists=(lgd,), bbox_inches='tight')
+	plt.close()
+
+def piechart(things, filename, title=None):
+	labels = sorted(things.keys())
+	counts = [ things[l] for l in labels ]
+	colours = [ get_colour(l) for l in labels ]
+
+	patches, texts = plt.pie(counts, colors=colours, startangle=90)
+	plt.legend(patches, collect.add_percent_labels(labels, counts), loc="best")
+	plt.axis('equal')
+	if title:
+		plt.suptitle(title, fontsize=14)
+	plt.tight_layout()
+	plt.savefig(filename)
 	plt.close()
--- lib/python/toys/collect.py
+++ lib/python/toys/collect.py
@@ -8,14 +8,29 @@ def add_percent_labels(labels, values):
 	total = sum(values)
 	return [ '{} ({:1.1f}%)'.format(l, 100 * (v / total)) for l, v in zip(labels, values) ]
 
-def collect_by(things, categorize):
-	results = collections.defaultdict(list)
-	for t in things:
-		results[categorize(t)].append(t)
-	return results
+def accumulate(things, sieve, count=lambda t: 1):
+	'''Collate things into categories and count (or accumulate) them
 
-def collect_and_count_by(things, categorize, count=lambda t: 1):
+	TODO: was called collect_and_count_by() in the not-yet-refactored code
+	'''
 	results = collections.defaultdict(int)
 	for t in things:
-		results[categorize(t)] += count(t)
+		results[sieve(t)] += count(t)
 	return results
+
+def collate(things, sieve):
+	'''Collate things into categories
+
+	TODO: was called collect_by() in the not-yet-refactored code
+	'''
+	results = collections.defaultdict(list)
+	for t in things:
+		results[sieve(t)].append(t)
+	return results
+
+def accumulate_2d(things, primary_sieve, secondary_sieve, count=lambda t: 1):
+	'''Collate the things in two different dimensions, then count them'''
+	data = collate(things, primary_sieve)
+	for k in data.keys():
+		data[k] = accumulate(data[k], secondary_sieve, count)
+	return data
