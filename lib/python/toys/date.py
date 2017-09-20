7ecabd5c463f1d57a160845cdcd7582ff4bfa060
Victor Chong <victor.chong@linaro.org>
2017-09-20 16:15:45 +0900

    date.py: Make date command work on OS X
    Signed-off-by: Victor Chong <victor.chong@linaro.org>
    [daniel.thompson@linaro.org] Update comment slightly
    Signed-off-by: Daniel Thompson <daniel.thompson@linaro.org>

--- lib/python/toys/date.py
+++ lib/python/toys/date.py
@@ -13,7 +13,18 @@ def smart_parse(s, end_of_day=False):
 	parser).
 
 	'''
-	date = subprocess.check_output(["date", "-d", s, "+%Y-%m-%d %H:%M:%S"])
+	try:
+		date = subprocess.check_output(["date", "-d", s, "+%Y-%m-%d %H:%M:%S"])
+	except subprocess.CalledProcessError as e:
+		'''
+		OS X date command does not follow GNU/Linux specifications
+		since it is Unix and will throw the CalledProcessError exception,
+		but we can make this work by installing the GNU date command as 'gdate'
+		using 'brew install coreutils'.
+
+		https://stackoverflow.com/questions/9804966/date-command-does-not-follow-linux-specifications-mac-os-x-lion
+		'''
+		date = subprocess.check_output(["gdate", "-d", s, "+%Y-%m-%d %H:%M:%S"])
 	date = str(date, 'utf-8')
 	date = date.rstrip()
 	date = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
