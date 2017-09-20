da6338aba6f768a735556df72f610ff77e862902
Victor Chong <victor.chong@linaro.org>
2017-09-20 15:11:00 +0900

    mymod

---
+++ run_2wksago.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+96btool pull --pipe | \
+96btool filter \
+	--since 'next friday -20 days' \
+	--until 'next friday -14 days' \
+	--user vchong | \
+96btool weekly
+
+96btool pull --pipe | \
+96btool filter \
+	--since 'next friday -20 days' \
+	--until 'next friday -14 days' \
+	--user vchong | \
+96btool worklog --time-spent 10h
+
+# NOT victor.chong@linaro.org/token
+ldtstool fetch --since 'next friday -20 days' | \
+ldtstool filter --until 'next friday -14 days' --assignee victor.chong@linaro.org | \
+ldtstool weekly
+
+ldtstool fetch --since 'next friday -20 days' | \
+ldtstool filter --until 'next friday -14 days' --assignee victor.chong@linaro.org | \
+ldtstool worklog --time-spent 10h
+
+glimpse weekly assignee = victor.chong@linaro.org > wkly_2wksago.txt
